265ca2595d8a4d153541756ad5f26bdca08ec87f
agakundle <boermeef@yahoo.com>
2014-04-29 09:51:13 +0200

    created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treefrank::Application.config.secret_key_base = 'a93e13ed716dc263d335ed36700bcdbf4db974403ae9ebae5ffcae6d82fd5eaedf563bcb46e569177302cd177c9de0790d89440a6d584d9f19075e4bd28e75b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Treefrank::Application.config.session_store :cookie_store, key: '_treefrank_session'
